78cf2d35a5b44fc158f11a6b253389d29589f650
miyoshu <miyoshu1127@gmail.com>
2024-07-11 00:00:26 +0900

    修正SQLのWHERE抜け

--- ecsite/src/com/diworksdev/ecsite/dao/MyPageDAO.java
+++ ecsite/src/com/diworksdev/ecsite/dao/MyPageDAO.java
@@ -13,7 +13,7 @@ public class MyPageDAO {
 	dbConnector.getConnection();
 	public ArrayList<MyPageDTO> getMyPageUserInfo (String item_transaction_id, String user_master_id) throws SQLException {
 		ArrayList<MyPageDTO> myPageDTO = new ArrayList<MyPageDTO>();
-		String sql = "SELECT  ubit.id, iit.item_name, ubit.total_price, ubit.total_count,ubit.pay, ubit.insert_date FROM user_buy_item_transaction ubit LEFT JOIN item_info_transaction iit ON ubit.item_transaction_id = iit.id ubit.item_transaction_id= ? AND ubit.user_master_id= ? ORDER BY insert_date DESC";
+		String sql = "SELECT  ubit.id, iit.item_name, ubit.total_price, ubit.total_count,ubit.pay, ubit.insert_date FROM user_buy_item_transaction ubit LEFT JOIN item_info_transaction iit ON ubit.item_transaction_id = iit.id WHERE ubit.item_transaction_id= ? AND ubit.user_master_id= ? ORDER BY insert_date DESC";
 
 try {
 	PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -38,8 +38,8 @@ try {
 	}
 	return myPageDTO;
 	}
-	public int buyItemHistoryDelete (String item_transaction_id, String user_master_id) throws SQLException
-{
+	public int buyItemHistoryDelete (String item_transaction_id, String user_master_id) throws SQLException{
+
 		String sql = "DELETE FROM user_buy_item_transaction WHERE item_transaction_id= ? AND user_master_id = ?";
 		PreparedStatement preparedStatement;
 		int result =0;
